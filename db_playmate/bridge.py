06541c2d8897cdd09d38ae76ccb8115d35afb0cb
Jesse Lingeman <lingeman@Jesses-MacBook-Pro.local>
2020-01-27 18:42:21 -0500

    Added prototype for bridge between products and added streaming file download features to databrary and streaming upload features to box

---
+++ db_playmate/bridge.py
@@ -0,0 +1,46 @@
+from box import get_client
+import toml
+from box import Box
+from databrary import Databrary
+from kobo import Kobo
+
+"""
+This is the bridge between Box and Kobo/Databrary.
+Used to transfer files between the three systems.
+"""
+
+class Bridge:
+    def __init__(self, db_username, kobo_base_url, kobo_token, box_session):
+        self.box = box_session
+        #self.kobo = Kobo(kobo_base_url, kobo_token)
+        self.db = Databrary(db_username)
+
+    def transfer_box_to_databrary(self, box_path, db_volume, db_container, rename_file=None):
+        """
+        Rename file is optional, if so specify the filename to use on databrary)
+        """
+        # First, get the file from box
+        # This returns a box file object, not the download
+        box_file = self.box.get_file(box_path)
+
+        # Now open an output stream into databrary
+        # TODO
+
+    def transfer_databrary_to_box(self, db_volume, db_container, db_asset, box_path):
+        """
+        Transfer a file from databrary to Box
+        """
+        file_stream, total_size, filename = self.db.download_asset_stream(db_volume, db_container, db_asset)
+        self.box.upload_file_stream(file_stream, box_path, total_size, filename)
+
+
+if __name__ == "__main__":
+
+    with open("env/config.toml") as config:
+        cfg = toml.load(config)
+        clid = cfg["box"]["client_id"]
+        clsec = cfg["box"]["client_secret"]
+
+    box = get_client(clid, clsec)
+    bridge = Bridge("jmlingeman@nyu.edu", "", "", box)
+    bridge.transfer_databrary_to_box(899, 36876, 174780, "PLAY-Project@/db_play_bridge_test")
