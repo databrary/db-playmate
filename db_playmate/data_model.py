c4ce6e4691757c91050a3ffaabf9d962057613a3
Jesse Lingeman <jmlingeman@gmail.com>
2020-02-20 21:57:47 -0500

    Added bridge support to pull files in from kobo, pull information out of databrary, and parse the databrary info.

--- db_playmate/data_model.py
+++ db_playmate/data_model.py
@@ -1,6 +1,14 @@
+import pickle
+import re
+
+
 class Submission:
-    def __init__(self, kobo_data):
-        self.kobo_data = kobo_data
+    def __init__(self, site_id, subj_number, db_asset):
+        self.asset = db_asset
+        self.vol_id = db_asset["vol_id"]
+        self.site_id = site_id
+        self.subj_number = subj_number
+        self.kobo_data = None
         self.ready_for_qa = False
         self.ready_for_coding = False
         self.moved_to_silver = False
@@ -9,18 +17,93 @@ class Submission:
         self.rel_coding_finished = False
         self.moved_to_gold = False
 
-    def video_exists(self):
-        # Check to see if the video is on DB
-        pass
+        # Lists because there can be multiple videos per
+        # task
+        self.natural_play = []
+        self.house_walk = []
+        self.home_question = []
+        self.structured_play = []
+        self.consent = []
+
+        self.video_map = {
+                "NaturalPlay": self.natural_play,
+                "StructuredPlay": self.structured_play,
+                "HouseWalkthrough": self.house_walk,
+                "Questionnaires": self.home_question,
+                "Consent": self.consent
+                }
 
     def _to_csv(self):
         # Return a CSV string rep
         pass
 
+    def __eq__(self, other):
+        if self.vol_id == other.vol_id and self.filename == other.filename:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        filenames = [x['filename'] for a in self.video_map.values() for x in a]
+        return "Asset: {} {} {} - Filenames: ".format(self.vol_id, self.site_id, self.subj_number) + " ".join(filenames)
+
+    def add_video(self, asset):
+        filename = asset['filename']
+        video_type = filename.split("_")[-1].split(".")[0]
+        if video_type[-1].isdigit():
+            video_type = video_type[:-1]
+        if video_type in self.video_map and asset not in self.video_map.values():
+            print(video_type)
+            self.video_map[video_type].append(asset)
+        else:
+            print("Warning: Could not find video_type", video_type, "in map or video was already in list")
+
+
 class Lab:
-    def __init__(self, site_code, lab_code, pi_fullname, email):
+    def __init__(self, site_code, lab_code, pi_fullname, email, institution):
         self.site_code = site_code
         self.lab_code = lab_code
         self.pi_fullname = pi_fullname
         self.email = email
         self.db_volume = "PLAYProject_" + site_code
+        self.institution = institution
+        self.vol_id = None
+
+    def __str__(self):
+        return "{} - {}".format(institution, site_code)
+
+
+    def assign_video(self, asset):
+        pass
+
+
+class Site:
+    def __init__(self, site_code):
+        self.site_code = site_code
+        self.db_volume = "PLAYProject_" + site_code
+        self.submissions = {}
+        self.labs = {}
+
+    def add_video(self, site_id, asset):
+        filename = asset['filename']
+        subj_number = filename.split("_")[2]
+        if subj_number not in self.submissions:
+            self.submissions[subj_number] = Submission(site_id, subj_number, asset)
+        self.submissions[subj_number].add_video(asset)
+
+    def get_vol_id(self, databrary_instance):
+        self.vol_id = databrary_instance.get_volume_by_name(self.db_volume)
+
+
+class Datastore:
+    def __init__(self):
+        self.labs = {} # lab_code -> lab
+        self.sites = {} # site_code -> site
+
+    def add_video(self, asset):
+        site_id = asset['filename'].split("_")[1].strip()
+        self.sites[site_id].add_video(site_id, asset)
+
+    def save(self, filename):
+        with open(filename, 'wb') as handle:
+            pickle.dump(self, handle)
--- db_playmate/frontend.py
+++ db_playmate/frontend.py
@@ -1,8 +1,69 @@
 import os
+import pickle
 from flask import Flask, render_template
+from sheets import read_master
+from data_model import Datastore, Submission
+from bridge import Bridge
+
+SAVE_FILE = "env/db_playmate.pickle"
+
 
 def check_for_new():
     # Check Kobo for a list of new files that need to be sent to QA
     pass
 
-def 
+
+def get_labs(bridge):
+    sites, labs = read_master()
+    for site in sites.values():
+        site.get_vol_id(bridge.db)
+    return sites, labs
+
+
+def get_submissions(sites, bridge, datastore):
+    for site in sites.values():
+        assets = bridge.db.get_assets_for_volume(site.vol_id)
+        if assets is not None:
+            for a in assets:
+                print(a)
+                if a['filename'].endswith(".mp4"):
+                    try:
+                        datastore.add_video(a)
+                        print("FOUND VIDEO!", a)
+                    except IndexError as e:
+                        print("Error adding video")
+                        print(e)
+
+
+def get_kobo_forms(bridge):
+    bridge.transfer_kobo_to_box()
+
+
+def startup():
+    # Set up the Flask server we'll be using
+
+    # Set up the DB/Box/Kobo bridge
+    bridge = Bridge("env/config.toml")
+
+    # Load the data if it exists, otherwise populate from
+    # online resources
+    if os.path.exists(SAVE_FILE):
+        with open(SAVE_FILE, 'rb') as handle:
+            datastore = pickle.load(handle)
+    else:
+        datastore = Datastore()
+        datastore.sites, datastore.labs = get_labs(bridge)
+        get_submissions(datastore.sites, bridge, datastore)
+
+        print(datastore.sites.keys())
+        #get_kobo_forms(bridge)
+        datastore.save(SAVE_FILE)
+
+        for site_code in datastore.sites:
+            print(datastore.sites[site_code].submissions.keys())
+            for subj in datastore.sites[site_code].submissions:
+                print(site_code, subj, datastore.sites[site_code].submissions[subj])
+
+
+if __name__ == "__main__":
+    startup()
