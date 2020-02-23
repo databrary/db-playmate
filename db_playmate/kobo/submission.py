c53aafd9b4ee282c0f345be85f4b12ffb1763798
Shohan Hasan <shohan.hasan@nyu.edu>
2020-02-23 18:00:55 -0500

    (feat) Update csv and dict representations for submissions

--- db_playmate/kobo/submission.py
+++ db_playmate/kobo/submission.py
@@ -3,17 +3,18 @@ from more_itertools import first
 
 class Submission:
     """
-    A collection of answers to questions.
+    A mapping of questions to answers
     """
 
     @classmethod
     def ignore_key(cls, key):
         return any(key.startswith(x) for x in ("_", "meta"))
 
-    def __init__(self, data, version=None, default_value=""):
+    def __init__(self, parent, data, version=None, default_value=""):
         self.version = version
         self.default_value = default_value
         self.data = {}
+        self.parent = parent
         for key, value in data.items():
             if self.ignore_key(key):
                 continue
@@ -23,9 +24,15 @@ class Submission:
                 raise Exception(f"Invalid key: {key}")
             self.data[qid] = value
 
-    def to_row_dict(self, questions):
-        return {str(q): self.answer(q) for q in questions}
+    def as_dict(self):
+        return {str(q): self.answer(q) for q in self.parent.questions}
 
     def answer(self, question):
         ans = first([self.data.get(name) for name in question.names.values()])
         return ans or self.default_value
+
+    def as_csv(self):
+        return ",".join(self.answer(q) for q in self.parent.questions)
+
+    def __repr__(self):
+        return self.as_csv()
