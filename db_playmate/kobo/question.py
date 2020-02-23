dcb63a84fb4e92d2a672f1682b32683a1b2e27ce
Shohan Hasan <shohan.hasan@nyu.edu>
2020-02-23 17:58:54 -0500

    (refact) Remove unused class and add alias checking method

--- db_playmate/kobo/question.py
+++ db_playmate/kobo/question.py
@@ -1,5 +1,5 @@
 class Question:
-    """Stores information about a form question; essentially sets of Kobo ids and question texts."""
+    """Represents a particular question on a form"""
 
     def __init__(self, qid, name, label=None, qtype=None):
         self.names = {qid: name}
@@ -11,21 +11,16 @@ class Question:
         self.names[qid] = name
         self.labels[qid] = label
 
+    def is_alias(self, other) -> bool:
+        if isinstance(other, int):
+            return other in self.names
+        elif isinstance(other, self.__class__):
+            return len(self.names & other.names) > 0
+        else:
+            raise NotImplemented
+
     def __str__(self):
         return self.names[self.__sid] or str(self.__sid)
 
-
-class QuestionGroup:
-    """
-    A group of related questions.
-    """
-
-    def __init__(self, group_id, *questions):
-        self.group_id = group_id
-        self.questions = list(questions)
-
-    def add_questions(self, question):
-        self.questions.append(question)
-
-    def __sizeof__(self):
-        return len(self.questions)
+    def __hash__(self):
+        return hash(self)
