9fb56e4c3b280f20c3544b809776557f89a285b9
Jesse Lingeman <jmlingeman@gmail.com>
2020-07-20 23:21:32 -0400

    Add filename for the datastore

--- db_playmate/constants.py
+++ db_playmate/constants.py
@@ -4,11 +4,11 @@ import os
 APP_NAME = "db-playmate"
 APP_AUTHOR = "Databrary"
 
-QA_DIR = ""
 PLAY_PREFIX = "PLAY-Project@/"  # For if there is a shared folder
 
+QA_CODING_DIR = PLAY_PREFIX + "automation_doNotTouch/2_PLAY_qa_opfs/1_PLAY_qa_templates"
 QA_CODED_DIR = (
-    PLAY_PREFIX + "automation_doNotTouch/2_PLAY_qa_opfs/2_PLAY_qa_complete/PLAY_QA_{}/"
+    PLAY_PREFIX + "automation_doNotTouch/2_PLAY_qa_opfs/2_PLAY_qa_complete/PLAY_QA_{}"
 )
 PRI_CODING_DIR = (
     PLAY_PREFIX
@@ -18,6 +18,13 @@ PRI_CODED_DIR = (
     PLAY_PREFIX
     + "automation_doNotTouch/4a_PLAY_coding_opf_templates_and_submissions/PLAY_opfs_{}/PLAY_opfs_{}/3_Submitted_CannotEditAnymore"
 )  # type / lab / 3_Submitted_CannotEditAnymore
+PRI_WORK_DIR = (
+    PLAY_PREFIX
+    + "automation_doNotTouch/4a_PLAY_coding_opf_templates_and_submissions/PLAY_opfs_{}/PLAY_opfs_{}/2_InProgress"
+)  # type / lab / 3_Submitted_CannotEditAnymore
+
+VIDEO_DIR = PLAY_PREFIX + "automation_doNotTouch/1_PLAY_videos_for_coding"
+
 
 REL_CODING_DIR = (
     PLAY_PREFIX
@@ -31,7 +38,10 @@ DEFAULT_SAVE_DIR = "env/db_playmate.pickle"
 
 USER_DATA_DIR = appdirs.user_data_dir(APP_NAME, APP_AUTHOR)
 TMP_DATA_DIR = appdirs.user_cache_dir(APP_NAME, APP_AUTHOR)
-GOLD_FINAL_DIR = PLAY_PREFIX + "automation_doNotTouch/7_PLAY_GOLD/{}/"
-SILVER_FINAL_DIR = PLAY_PREFIX + "automation_doNotTouch/8_PLAY_SILVER/{}/{}/"
+SAVE_FILE_NAME = USER_DATA_DIR + "/" + "db_playmate.pickle"
+GOLD_FINAL_DIR = PLAY_PREFIX + "automation_doNotTouch/7_PLAY_GOLD/{}/"  # Site
+SILVER_FINAL_DIR = (
+    PLAY_PREFIX + "automation_doNotTouch/8_PLAY_SILVER/{}/{}/"
+)  # Site / Pass
 os.makedirs(USER_DATA_DIR, exist_ok=True)
 os.makedirs(TMP_DATA_DIR, exist_ok=True)
